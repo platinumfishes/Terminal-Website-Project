086378036830e3e863e548b5ba16d4b539a908b8
Platinumfish <mulerotmglel@gmail.com>
2023-01-06 21:10:12 -0500

    8

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 var commands = document.getElementById("commands");
 var commandline = document.getElementById("user_input");
-
+var terminal_outputs = document.getElementById("terminal_outputs");
 
 
 /* BUTTON OPTION BELOW
@@ -14,8 +14,13 @@ temp_btn.addEventListener("click", function(){
     commandline.value = "";
 });
 */
-
+ 
 function execute(input){
+    let output;
+    input = input.toLowerCase();
+    output = `<div>testing the input is ${input}</div>`; 
+    terminal_outputs.innerHTML = `${terminal_outputs.innerHTML+output}`
+
     var newCommand = document.createElement("li");
     newCommand.innerHTML = input;
     commands.appendChild(newCommand); 
