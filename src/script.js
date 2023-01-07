a644eaefa3b2d26d2619f14207d8589143737bcc
Platinumfish <mulerotmglel@gmail.com>
2023-01-06 20:46:35 -0500

    6

--- src/script.js
+++ src/script.js
@@ -1,10 +1,30 @@
 var commands = document.getElementById("commands");
-var commandline = document.getElementById("commandline");
-var temp_btn = document.getElementById("temp_btn")
+var commandline = document.getElementById("user_input");
+/* var temp_btn = document.getElementById("temp_btn") */
 
 temp_btn.addEventListener("click", function(){
     var newCommand = document.createElement("li");
     newCommand.innerHTML = commandline.value;
     commands.appendChild(newCommand);  
     commandline.value = "";
-}); 
+});
+
+function execute(input){
+    var newCommand = document.createElement("li");
+    newCommand.innerHTML = input;
+    commands.appendChild(newCommand); 
+}
+
+function key(e){
+    const input = user_input.innerHTML;
+
+    if(e.key == "Enter"){
+        execute();
+        user_input.value = "";
+        return;
+    }
+
+    user_input.innerHTML = input + e.key;
+}  
+
+document.addEventListener("keypress", key);
