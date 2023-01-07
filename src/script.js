9f2a4d9362144d0f01af9bf0112a2a83c36307a8
Platinumfish <mulerotmglel@gmail.com>
2023-01-06 21:24:49 -0500

    12

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ var terminal_outputs = document.getElementById("terminal_outputs");
 function execute(input){
     let output;
     input = input.toLowerCase();
-    output = `<div>testing the input is ${input}</div>`; 
+    output = `<div>➤ The input is "${input}"</div>`; 
     terminal_outputs.innerHTML = `${terminal_outputs.innerHTML+output}`
 }
 
